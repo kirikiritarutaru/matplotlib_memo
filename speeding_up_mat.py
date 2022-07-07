19283245aecaff7cc2c175651d53de10a1f448f3
tarutaru <kirikia@gmail.com>
2022-07-07 23:18:09 +0900

    積み上げ横グラフ表示追加

--- speeding_up_mat.py
+++ speeding_up_mat.py
@@ -1,6 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 def init_mpl(x_range, y_range, figsize=(6, 6)):
@@ -14,9 +15,9 @@ def init_mpl(x_range, y_range, figsize=(6, 6)):
     return fig, ax, plt_bg
 
 
-if __name__ == '__main__':
+def fast_mpl():
     fig, ax, plt_bg = init_mpl(x_range=[0, 6], y_range=[-1, 1])
-    x = np.linspace(0, 2*np.pi, 100)
+    x = np.linspace(0, 2 * np.pi, 100)
     (ln, ) = ax.plot(x, np.sin(x), animated=True)
     plt.show(block=False)
     plt.pause(0.1)
@@ -34,3 +35,62 @@ if __name__ == '__main__':
         floor_plot = cv2.cvtColor(
             np.array(fig.canvas.renderer.buffer_rgba()), cv2.COLOR_RGBA2BGR
         )
+
+
+def get_data():
+    data = np.array(
+        [[156, 128, 285], [286, 220, 297], [218, 235, 137], [160, 298, 112]]
+    )
+    row_labels = ["2017", "2018", "2019", "2020"]
+    col_labels = ["Android", "iOS", "Windows"]
+
+    return pd.DataFrame(data, index=row_labels, columns=col_labels)
+
+
+def vis_stacked_graph():
+    df = get_data()
+
+    n_rows, n_cols = df.shape
+    positions = np.arange(n_rows)
+    offsets = np.zeros(n_rows, dtype=df.values.dtype)
+    colors = plt.get_cmap("tab20c")(np.linspace(0, 1, n_cols))
+
+    fig, ax = plt.subplots()
+    ax.set_yticks(positions)
+    ax.set_yticklabels(df.index)
+    ax.set_xlim([0, 800])
+    ax.set_ylim([-1, 4])
+
+    fig.canvas.draw()
+    bg = fig.canvas.copy_from_bbox(fig.bbox)
+
+    fig.canvas.restore_region(bg)
+
+    for i in range(len(df.columns)):
+        # 棒グラフを描画する。
+        bar = ax.barh(positions, df.iloc[:, i], left=offsets, color=colors[i])
+        offsets += df.iloc[:, i]
+
+        # 棒グラフのラベルを描画する。
+        for rect in bar:
+            cx = rect.get_x() + rect.get_width() / 2
+            cy = rect.get_y() + rect.get_height() / 2
+            ax.draw_artist(ax.add_patch(rect))
+            text_ax = ax.text(
+                cx, cy, df.columns[i], color="k", ha="center", va="center", animated=True
+            )
+            ax.draw_artist(text_ax)
+
+    fig.canvas.blit(fig.bbox)
+    fig.canvas.flush_events()
+    np_plot = cv2.cvtColor(
+        np.array(fig.canvas.renderer.buffer_rgba()),
+        cv2.COLOR_RGBA2BGR
+    )
+
+    cv2.imshow('plot', np_plot)
+    cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    vis_stacked_graph()
